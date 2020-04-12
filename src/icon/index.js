914b0bb0c561baf5469198f9c727be4b2ce70316
lxq <berrylxq@qq.com>
2020-04-12 23:51:21 +0800

    style(优化样式): 优化样式

---
+++ src/icon/index.js
@@ -0,0 +1,8 @@
+import error from './error.svg';
+import ok from './ok.svg';
+import stop from './stop.svg';
+import info from './info.svg';
+import alert from './alert.svg';
+export default {
+    error, ok, stop, info, alert
+}
