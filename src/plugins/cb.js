a11c5b960ed99bc14121e559a16ee9e8240dd15e
void <liangxiaoqiang@techsight.cn>
2020-07-30 11:33:15 +0800

    fix: take eventpool to instance

--- src/plugins/cb.js
+++ src/plugins/cb.js
@@ -1,19 +1,24 @@
 import addPlugin from "../core/plugin";
-
-const pool = new Map();
+function fillPool() {
+  this._pool = this._pool || new Map();
+}
 function onPlugin(type, fn) {
-  pool.set(type, pool.get(type) || new Set());
-  pool.get(type).add(fn);
+  fillPool.call(this);
+  this._pool.set(type, this._pool.get(type) || new Set());
+  this._pool.get(type).add(fn);
 }
 function offPlugin(type, fn) {
-  const set = pool.get(type);
+  fillPool.call(this);
+
+  const set = this._pool.get(type);
   if (!set) {
     return;
   }
   fn ? set.delete(fn) : set.clear();
 }
 function emitPlugin(type, data) {
-  const set = pool.get(type);
+  fillPool.call(this);
+  const set = this._pool.get(type);
   if (set) {
     for (const fn of set.values()) {
       fn.call(this, data);
