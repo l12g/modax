10babb5a4fa8cf4e2f0cae8d2cfc5704d48d638e
void <liangxiaoqiang@techsight.cn>
2020-07-30 11:36:28 +0800

    fix: fix prompt bug

--- src/plugins/prompt.js
+++ src/plugins/prompt.js
@@ -10,6 +10,17 @@ const DEFAULT_PROMPT_CONFIG = {
 function promptPlugin(opt = DEFAULT_PROMPT_CONFIG) {
   this._content = "";
   this._prompt = opt;
+  this.on("show", () => {
+    this._inputVal = (opt.value + "").length > 0 ? opt.value : "";
+    const input = this._el.querySelector("input,textarea");
+    if (input) {
+      input.focus();
+      input.value = this._inputVal || "";
+      input.oninput = () => {
+        this._inputVal = input.value;
+      };
+    }
+  });
   return this;
 }
 // adv plugins
