adf870491315e396c0291d8494f90fe1f1006915
void <liangxiaoqiang@techsight.cn>
2020-07-30 11:34:30 +0800

    feat: icon support

--- src/core/html.js
+++ src/core/html.js
@@ -1,4 +1,3 @@
-
 const html = `
 <div class='mx' _style='{width}'>
     <div class='mx__title' _text='title' _visible='!!title'></div>
@@ -7,22 +6,22 @@ const html = `
             <div class='mx__content-icon' _visible='!!icon &&  !prompt' _style='{color:iconColor}' _text='icon'></div>
             <div class='mx__content-raw' _visible='!!content && !prompt' _text='content'></div>
             <div _visible='!!prompt' class='mx__prompt'>
-                <input class='mx__input' _visible='!prompt.multline' autofocus='autofocus' on-input='onInputChange'/>
-                <textarea class='mx__input' _visible='prompt.multline' autofocus='autofocus' on-input='onInputChange' _rows='prompt.rows'></textarea>
+                <input class='mx__input' _visible='!prompt.multline' autofocus='autofocus'/>
+                <textarea class='mx__input' _visible='prompt.multline' autofocus='autofocus' _rows='prompt.rows'></textarea>
             </div>
         </div>
     </div>
     <div class='mx__footer'>
         <div class='mx__btn' 
-        on-click='item.click'
         _each='item of actions' 
         _visible='item.visible'
-        _class='["mx__btn--"+item.type,item.loading?"disabled":""]' 
+        _class='["mx__btn--"+item.type,item.loading||item.disabled?"disabled":""]' 
+        _data-id='item.id'
         _key='item.key'>
-            <div class='mx-loading' _visible='!!item.loading'><span/></div>
+            <span class='mx-loading' _visible='!!item.loading'></span>
             <span _text='item.text'></span>
         </div>
     </div>
 </div>
-`
+`;
 export default html;
